c1a20063ab2f7962b7b457452f8239aa63042437
Fillipe Paz <Fillipe Paz@WIN>
2016-03-11 19:11:15 -0300

    commit

---
+++ src/br/ufs/dcomp/banco/AppBanco.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fillipe Almeida e Carolina Louzada
+ */
+package br.ufs.dcomp.banco;
+
+import br.ufs.dcomp.banco.privado.*;
+
+public class AppBanco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
