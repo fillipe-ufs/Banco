a2c961d88ec9529625094a18f616f60907cfdd1f
CarolinaSL <carolinasantanalouzada@gmail.com>
2016-03-16 15:44:42 -0300

    Update Conta.java
    Versão para javadocs

--- src/br/ufs/dcomp/banco/privado/Conta.java
+++ src/br/ufs/dcomp/banco/privado/Conta.java
@@ -1,28 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.ufs.dcomp.banco.privado;
 
 import java.util.List;
 import java.util.ArrayList;
 
 /**
- *
+ * Classe que molda as contas associadas aos clientes de um banco.
  * @author Fillipe Paz
  */
 public class Conta {
-
-    private List<Cliente> cliente;
-    private Extrato extrato;
+    // Declaração de atributos e instanciação de objetos.
+    private List<Cliente> cliente; // Somente objetos do tipo Cliente podem adicionados à lista de clientes.
+    private Extrato extrato; // criação de um atributo do tipo extrato para cada conta
     private double saldo;
     private double limite;
     private int codigo; // Precisa ser modificado de modo a zerar de agência para agência.
 
+    /** Construtor inicializado para instanciação de uma conta associada a um cliente.*/
     public Conta(double saldo, Cliente cliente, double limite, int codigo) {
 
-        this.cliente = new ArrayList<>();
+        this.cliente = new ArrayList<>(); // O cliente vai para a lista do tipo Cliente.
         
         extrato = new Extrato();
         this.saldo = saldo;
@@ -32,7 +29,10 @@ public class Conta {
     }
 
   
-
+    /**  Método para sacar uma quantia da conta. Caso o valor pedido seja maior que o existente na conta, não é permitido o saque.
+     * @param double quantia
+     * @return boolean true ou false
+    */
     public boolean Sacar(double quantia) {
 
         if (quantia > (limite + saldo)) {
@@ -49,13 +49,17 @@ public class Conta {
                 saldo = saldo - quantia;
             }
 
-            extrato.guardarSaque(this.saldo + this.limite, quantia);
-
-            return true;
+            extrato.guardarSaque(this.saldo + this.limite, quantia); // chama-se o método guardarSaque para guardar o valor do saldo inicial e final
+            return true;                                             // em extrato
 
         }
     }
-
+    /** Método para realizar pagamentos. O pagamento é realizado a partir da conta. Através de um código de pagamento o valor 
+     * é retirado da conta. Caso o valor existente seja menor que o valor do pagamento uma mensagem é retornada.
+     * @param Double quantia
+     * @param String codigo
+     * @return boolean true ou false
+    */
     public boolean RealizarPagamento(Double quantia, String codigo) {
 
         if (quantia > (limite + saldo)) {
@@ -81,15 +85,17 @@ public class Conta {
     public double getSaldo() {
         return saldo;
     }
-
-    public boolean transferir(Conta contaDestino, double quantia) {
-        // precisa refinar os critérios e impedir a transferencia entre bancos diferentes
+    /** Método para transferência de valores entre contas.
+     * @param Conta contaDestino - Conta cuja quantia será enviada.
+     * @param double quantia
+    */
+    public boolean transferir(Conta contaDestino, double quantia){
             if (quantia > (saldo + limite)) {
                 System.out.println("A transferência não pode ser concluída, pois a quantia ultrapassa o valor disponível para o cliente");
             } else {
-                extrato.guardarTransferencia(saldo, quantia, contaDestino);
-                contaDestino.extrato.guardarTransferenciaContaDestino(quantia, contaDestino, cliente.get(0).getNome());
-                contaDestino.setSaldo(contaDestino.getSaldo()+quantia);
+                extrato.guardarTransferencia(saldo, quantia, contaDestino); // extrato capta o saldo da conta, o valor da transferencia e o número da conta de destino
+                contaDestino.extrato.guardarTransferenciaContaDestino(quantia, contaDestino, cliente.get(0).getNome()); // informações copiadas para o extrato da conta de destino
+                contaDestino.setSaldo(contaDestino.getSaldo()+quantia); // modificação do saldo da conta destinada
                 this.saldo = this.saldo - quantia;
                 
 
@@ -98,17 +104,23 @@ public class Conta {
     }
 
         
-
+    /**Metodo para geração de um extrato das operações de uma conta.
+     * @ return Extrato extrato 
+     */
     public Extrato gerarExtrato() {
         //Dúvida: Carol, fazer o print das informações no método ou enviá-lo para o AppBanco? Estou em
         //dúvida se é possível exibir lá sem a necessidade de instanciar um objeto do tipo Extrato.
         return extrato;
 
     }
-
+    /**  Método para buscar um cliente a partir de seu RG. Serve para qualquer cliente, de qualquer agência e 
+    * considera que um cliente pode ter mais de uma conta.
+    * @param String rg
+    * @return List clienteBusca 
+    */
     public List buscarCliente(String rg) {
 
-        List<Cliente> clienteBusca;
+        List<Cliente> clienteBusca; // nomeia um arrayList do tipo Cliente para captar o cliente cujo RG confere.
         clienteBusca = new ArrayList<>();
 
         for (int i = 0; i < cliente.size(); i++) {
