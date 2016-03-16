87827244706680b82b328708125b26eb07e2c6fc
CarolinaSL <carolinasantanalouzada@gmail.com>
2016-03-16 17:19:19 -0300

    Update Banco.java

--- src/br/ufs/dcomp/banco/privado/Banco.java
+++ src/br/ufs/dcomp/banco/privado/Banco.java
@@ -1,19 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.ufs.dcomp.banco.privado;
 
 import java.util.List;
 import java.util.ArrayList;
 
 /**
- *
+ * Molda as operações da classe Banco.
  * @author Fillipe Paz
  */
 public class Banco {
-
+    // declaração de atributos e lista de agências.
     private static int codigoBancos;
     private int codigoAgencias = 1; 
     private int codigo;
@@ -22,6 +18,8 @@ public class Banco {
     private double montante;
     private List<Agencia> agencias;
 
+    /** Construtor que recebe um nome e uma sigla para cada banco cadastrado. 
+    */
     public Banco(String sigla, String nome) {
         this.nome = nome;
         this.sigla = sigla;
@@ -29,23 +27,30 @@ public class Banco {
         codigo = codigoBancos;
         incrementarCodigoBancos();
     }
-
+    /**  Método sem retorno ou parâmetro, apenas para incrementar o código para cada agência criada em um banco.
+    */
     private void incrementarCodigoAgencia() {
         codigoAgencias++;
     }
-    
+    /**  Método sem retorno ou parâmetro, apenas para incrementar o código para cada banco criado.
+    */
     private void incrementarCodigoBancos()
     {
         codigoBancos++;
     }
-
+    /**  Método para retornar montante total de um banco através da soma dos montantes das agências de cada banco.
+     * @return double montante
+    */
     public double getMontante() {
         for (Agencia agencia : agencias) {
             montante = agencia.getMontanteAgencia();
         }
         return montante;
     }
-
+    /**  Método que retorna o saldo total do cliente de todas as agências daquele banco através do número do RG.
+     * @param String rg
+     * @return double saldoCliente
+    */
     public double getSaldoCliente(String rg) {
         double saldoCliente = 0;
         if (Cliente.verificaRG(rg) == false) {
@@ -73,7 +78,10 @@ public class Banco {
     public String getNome() {
         return nome;
     }
-
+    /** Método para encontrar contas determinadas de uma agência em específico. 
+     * @param int codAgencia
+     * @return codConta
+    */
     public List encontrarConta(int codAgencia, int codConta) {
         List<Conta> contaBuscada;
         contaBuscada = new ArrayList<>();
@@ -89,7 +97,8 @@ public class Banco {
         return contaBuscada; // Tratamento de informação necessário. 
         //Se a lista estiver vazia, logo a conta não existe. 
     }
-
+    /** Método que adiciona uma nova agência ao Banco. 
+    */
     public void criarAgencia()
     {
         Agencia agencia = new Agencia(codigoAgencias);
