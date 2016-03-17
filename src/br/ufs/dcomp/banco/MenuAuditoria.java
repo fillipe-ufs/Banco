05d2d67293d251ae2f6dcffec0b6873376f9cf34
CarolinaSL <carolinasantanalouzada@gmail.com>
2016-03-17 16:44:58 -0300

    Update MenuAuditoria.java

--- src/br/ufs/dcomp/banco/MenuAuditoria.java
+++ src/br/ufs/dcomp/banco/MenuAuditoria.java
@@ -19,15 +19,14 @@ public class MenuAuditoria {
         int numeroBanco;
         int contadorBanco;
         int numeroAgencia;
+        String rg;
         
         System.out.println(" Bem vindo ao menu de Gestão para Auditoria!\n O que gostaria de fazer?");
         System.out.println(" Digite:");
         System.out.println(" 1 - Para cadastrar um banco + " 
                          + " 2 - Para cadastrar uma agência."
-                         + " 3 - Para verficar o Montante do Banco.\n"
-                         + " 4 - Para verificar o Montante de uma Agência."
-                         + " 5 - Para verificar o saldo total de um cliente no banco.\n"
-                         + " 6 - Para verificar o saldo total de um cliente em uma agência.");
+                         + " 3 - Para verificar o Montante de um Banco ou de uma agência.\n"
+                         + " 4 - Para verificar o saldo total de um cliente em um banco ou em uma agência.\n");
         opcao = ler.nextShort();
         switch(opcao){
             
@@ -55,6 +54,7 @@ public class MenuAuditoria {
                 for( int i = 0; i < auditoria.bancos.size();i++){
                     if(numeroBanco == auditoria.bancos.get(i).getCodigo())
                         System.out.println(" Montante do banco:" + auditoria.bancos.get(i).getMontante());
+                        
                 }
                 System.out.println(" Gostaria de verificar o montante de uma agência deste banco?"
                                     + " Responda 1 para sim ou 0  para não. ");
@@ -62,18 +62,47 @@ public class MenuAuditoria {
                 if( resposta == 1){
                     System.out.println(" Digite o número da agência:");
                     numeroAgencia = ler.nextInt();
-                    auditoria.bancos.get()
+                    for( int j = 0; j < auditoria.agencias.size();j++){
+                        if(numeroAgencia == auditoria.agencias.get(j).getCodigo()){
+                            System.out.println(" Montante da agência especificada: \nR$ " 
+                                                + auditoria.agencias.get(j).getMontanteAgencia());
+                        }
                     
                     }
-                    
+                } else {
+                    System.out.println(" Obrigada!"); // Usar return?
+                }
                 break;
-            case 4 :
                 
-                
-            case 5 :
-                
-            case 6 :
-            
+            case 4: 
+                    System.out.println(" Digite o código do banco.");
+                    numeroBanco = ler.nextInt();
+                    System.out.println(" Digite o RG do cliente cujo saldo total deseja obter:");
+                    rg = ler.next();
+                    for( int i = 0; i < auditoria.bancos.size(); i++){ // consertar a partir daqui
+                        if(numeroBanco == auditoria.bancos.get(i).getCodigo())
+                        System.out.println(" Saldo do cliente no banco:" 
+                                        + auditoria.bancos.get(i).getSaldoCliente(rg));// até aqui
+                    }
+                    System.out.println(" Gostaria de obter o saldo do cliente em uma agência específica?");
+                    System.out.println(" Digite 1 para sim e 0 para não.");
+                    resposta = ler.nextInt();
+                    
+                    if( resposta == 1){
+                        System.out.println(" Digite o código da agência:");
+                        numeroAgencia = ler.nextInt();
+                        for( int i = 0; i < auditoria.bancos.size(); i++){ // verifica a partir daqui
+                            if( numeroBanco == auditoria.bancos.get(i).getCodigo()){
+                                if(numeroAgencia == auditoria.agencias.get(i).getCodigo()){
+                                    System.out.println(" Saldo do cliente na agência:"
+                                    + auditoria.agencias.get(i).getSaldoCliente(rg)); // até aqui
+                                }
+                            }
+                        }
+                    }
+                    break;
+                    
+              
         }
     }
 }
